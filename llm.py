df244cdab4d79297ff34d59d5200b3a2cf999513
hyeridfd <160723285+hyeridfd@users.noreply.github.com>
2025-04-05 22:57:28 +0900

    Update llm.py

--- llm.py
+++ llm.py
@@ -5,7 +5,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
-
+import streamlit as st
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
