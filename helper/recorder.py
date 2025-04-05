a3bfef23cfbba91bffc3a2d2dbc45128cb2ff0bd
hyeridfd <160723285+hyeridfd@users.noreply.github.com>
2025-04-05 19:31:46 +0900

    Update recorder.py

--- helper/recorder.py
+++ helper/recorder.py
@@ -1,5 +1,3 @@
-from pyrecorder.recorder import Recorder
-from pyrecorder.writers.video import Video
 from pymoo.visualization.scatter import Scatter
 
 # 최적화 과정(진화 과정)을 시각적으로 기록하는 함수 -> 세대별 진화 과정이 담긴 .mp4 영상 파일임(알고리즘이 어떻게 해를 점점 더 최적화해 나가는지를 애니메이션으로 보여줌)
