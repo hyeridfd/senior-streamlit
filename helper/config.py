75ca882ac7e5b2abc66c416691f4df7af8932eae
hyeridfd <160723285+hyeridfd@users.noreply.github.com>
2025-04-09 11:42:25 +0900

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -91,7 +91,7 @@ class Config:
                     'protein': (self.PROTEIN * 0.9, self.PROTEIN * 1.5),
                     'fat': (self.FAT * 0.9, self.FAT * 1.5),
                     'cho': (self.CHO * 0.9, self.CHO * 1.5)
-            }), 'weight': 0.5
+            }), 'weight': 0.8
             },
             {
                 'function': ChewingStageFitness(self.CHEWING_STAGE),
@@ -99,11 +99,11 @@ class Config:
             },
             {
                 'function': MainIngredientFitness(),
-                'weight': 0.2
+                'weight': 0.05
             },
             {
                 'function': RepetitionFitness(),
-                'weight': 0.2
+                'weight': 0.05
             },
             {
                 'function': PreferenceFitness(self.PREFERENCE),
