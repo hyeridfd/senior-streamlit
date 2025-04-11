fc974b4f6a71d3c44c7e33ae8dc6eb56ee95e5f1
hyeridfd <160723285+hyeridfd@users.noreply.github.com>
2025-04-11 09:58:12 +0900

    Update menu_planning_problem.py

--- pymoo_operators/menu_planning_problem.py
+++ pymoo_operators/menu_planning_problem.py
@@ -27,8 +27,7 @@ class MenuPlanningProblem(ElementwiseProblem):
             else:
                 self.df = pd.read_csv(file_path, encoding='utf-8')
         except Exception as e:
-            pass
-            #print("❌ CSV 파일을 불러오는 중 에러 발생:", e)
+            print("❌ CSV 파일을 불러오는 중 에러 발생:", e)
             sys.exit(1)
 
         user_chewing_stage = self.conf.CHEWING_STAGE  # ✅ 사용자 저작단계 가져오기
